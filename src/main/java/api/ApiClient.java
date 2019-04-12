5f24a5d2173d97e12ab49f6e40b61d894a4971bd
bluesocks <s42415466@gmail.com>
2019-04-12 21:22:47 +0900

    Add restTemplate variable

--- src/main/java/api/ApiClient.java
+++ src/main/java/api/ApiClient.java
@@ -1,8 +1,13 @@
 package api;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 public class ApiClient {
+    @Autowired
+    private RestTemplate restTemplate;
+
     
 }
