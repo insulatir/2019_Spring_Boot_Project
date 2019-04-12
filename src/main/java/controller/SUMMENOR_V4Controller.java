fa6d1b6c276d3ced68b6f060b16001ec8f0ad7c2
bluesocks <s42415466@gmail.com>
2019-04-12 21:06:27 +0900

    Add element summenor_v

--- src/main/java/controller/SUMMENOR_V4Controller.java
+++ src/main/java/controller/SUMMENOR_V4Controller.java
@@ -1,16 +1,21 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import service.SUMMENOR_V$Service;
 
 @RestController
 @RequestMapping("/lol/summoner/v4/summoners")
 public class SUMMENOR_V4Controller {
+    @Autowired
+    private SUMMENOR_V$Service summenor_v$Service;
+
     @GetMapping("/lol/summoner/v4/summoners/by-name/{summonerName}")
     public String getIdByName(@PathVariable String name) {
-
+        
     }
 }
 
