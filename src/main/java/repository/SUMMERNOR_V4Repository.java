d3fd40d08439bbd4e02872ee229f32fa1c41ed39
bluesocks <s42415466@gmail.com>
2019-04-12 21:39:30 +0900

    Add mongoTemplate variable

--- src/main/java/repository/SUMMERNOR_V4Repository.java
+++ src/main/java/repository/SUMMERNOR_V4Repository.java
@@ -1,8 +1,12 @@
 package repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class SUMMERNOR_V4Repository {
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
     
 }
