56eb23ee559edfdf7a5c12c719503a68e524d6d7
bluesocks <s42415466@gmail.com>
2019-04-12 21:32:13 +0900

    Add getNames method in AppClient class

--- src/main/java/api/ApiClient.java
+++ src/main/java/api/ApiClient.java
@@ -1,8 +1,13 @@
 package api;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
+
+import static org.springframework.http.HttpMethod.*;
 
 @Service
 public class ApiClient {
@@ -12,5 +17,9 @@ public class ApiClient {
     private final String names = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name";
     private final ParameterizedTypeReference<List<String>> responseType = new ParameterizedTypeReference<List<String>>() {};
 
-    
+    public List<String> getNames() {
+        List<String> responseBody = restTemplate.exchange(names, GET, null, responseType)
+                .getBody();
+        return responseBody;
+    }
 }
