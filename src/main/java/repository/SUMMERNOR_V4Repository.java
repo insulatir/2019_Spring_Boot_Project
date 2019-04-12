a3557146a71d1590e241eebced6481174f093cb0
bluesocks <s42415466@gmail.com>
2019-04-12 21:49:00 +0900

    Add findIdByName to SUMMERNOR_V4Repository class

--- src/main/java/repository/SUMMERNOR_V4Repository.java
+++ src/main/java/repository/SUMMERNOR_V4Repository.java
@@ -1,12 +1,24 @@
 package repository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.core.query.Query;
+import service.SUMMENOR_V$Service;
 
 @Repository
 public class SUMMERNOR_V4Repository {
     @Autowired
     private MongoTemplate mongoTemplate;
 
-    
+    public String findIdByName(Stinrg name) {
+        Query query = new Query();
+
+        query.addCriteria(Criteria.where("name").is(name));
+        query.with(Sort.by(Sort.Order.desc("id")));
+
+        
+    }
 }
