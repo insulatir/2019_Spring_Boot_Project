2c3323a4f2b3e95647d8f53672d1801168c8c660
bluesocks <s42415466@gmail.com>
2019-04-14 15:01:22 +0900

    Add 'R' to class Name in repository

--- src/main/java/com/the23rdTeam/controller/SUMMENOR_V4Controller.java
+++ src/main/java/com/the23rdTeam/controller/SUMMENOR_V4Controller.java
@@ -1,20 +1,20 @@
-package controller;
+package com.the23rdTeam.controller;
 
+import com.the23rdTeam.service.SUMMERNOR_V4Service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import service.SUMMENOR_V$Service;
 
 @RestController
 @RequestMapping("/lol/summoner/v4/summoners")
-public class SUMMENOR_V4Controller {
+public class SUMMERNOR_V4Controller {
     @Autowired
-    private SUMMENOR_V$Service summenor_v$Service;
+    private SUMMERNOR_V4Service summenor_v$Service;
 
     @GetMapping("/lol/summoner/v4/summoners/by-name/{summonerName}")
-    public String getIdByName(@PathVariable String name) {
+    public String getIdByName(@PathVariable String name, @PathVariable String summonerName) {
         return summenor_v$Service.getIdByName(name);
     }
 }
