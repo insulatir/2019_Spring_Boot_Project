c5ce6d4ea5c1c3f3773ad078e480efa4e28ba359
bluesocks <s42415466@gmail.com>
2019-04-14 15:05:29 +0900

    Delete unused argument

--- src/main/java/com/the23rdTeam/controller/SUMMENOR_V4Controller.java
+++ src/main/java/com/the23rdTeam/controller/SUMMENOR_V4Controller.java
@@ -1,6 +1,6 @@
 package com.the23rdTeam.controller;
 
-import com.the23rdTeam.service.SUMMERNOR_V4Service;
+import com.the23rdTeam.repository.SUMMERNOR_V4Repository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/lol/summoner/v4/summoners")
-public class SUMMERNOR_V4Controller {
+public class SUMMENOR_V4Controller {
     @Autowired
-    private SUMMERNOR_V4Service summenor_v$Service;
+    private SUMMERNOR_V4Repository summernor_v4Repository;
 
     @GetMapping("/lol/summoner/v4/summoners/by-name/{summonerName}")
-    public String getIdByName(@PathVariable String name, @PathVariable String summonerName) {
-        return summenor_v$Service.getIdByName(name);
+    public String getIdByName(@PathVariable String summonerName) {
+        return summernor_v4Repository.findIdByName(summonerName);
     }
 }
 
